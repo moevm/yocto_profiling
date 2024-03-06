5ffa91930589335a8b4c88909459d90028059ba9
Denisova Olga <90854310+denisova-ok@users.noreply.github.com>
2024-03-06 23:24:12 +0300

    upd make-charts.py

--- packages-charts/make-charts.py
+++ packages-charts/make-charts.py
@@ -2,17 +2,17 @@ import os
 import matplotlib.pyplot as plt
 
 
-def create_fs_tree(path='../../build/tmp/buildstats/20240212085739'):
+def create_fs_tree(path):
     tree = os.walk(path)
     return tree
 
 
-def create_chart(tree):
+def create_chart(tree, timestamp_list):
     metrics = ['utime', 'stime', 'cutime', 'cstime', 'IO rchar', 'IO wchar', 'rusage ru_utime', 'rusage ru_stime', 'rusage ru_maxrss', 'Child rusage ru_utime', 'Child rusage ru_stime'] #можно что-то добавить
     num_metrics = len(metrics)
     
     for i in range(num_metrics):
-        data = create_data(metrics[i], tree)
+        data = create_data(metrics[i], tree, timestamp_list)
         sorted_data = dict(sorted(data.items(), key=lambda item: item[1]))
         plt.figure(figsize=(10, 70))
         plt.barh(list(sorted_data.keys()), list(sorted_data.values()))
@@ -22,7 +22,7 @@ def create_chart(tree):
         plt.savefig(metrics[i]+'.png')
     
 
-def create_data(metric, tree):
+def create_data(metric, tree, timestamp_list):
     result = dict()
     for package in tree:
             data = 0
@@ -33,16 +33,37 @@ def create_data(metric, tree):
                             for line in file:
                                 if line.startswith(metric + ': '):
                                     data += float(line[line.index(':') + 2 : ])
-            if (not package[0].endswith('reduced_proc_pressure')) and (not package[0].endswith('20240212085739')):
+            if (not package[0].endswith('reduced_proc_pressure')) and (not (package[0].split('/'))[-1] in timestamp_list):
                 result.update({package[0][package[0].rfind('/') + 1 :] : data})
     return result
 
 
 def main():
-    tree = list(create_fs_tree())
-    create_chart(tree)
+    timestamp = ''
+    tree = list(os.walk('poky/build/tmp/buildstats'))
+    for item in tree:
+        if item[0] == 'poky/build/tmp/buildstats':
+            timestamp_list = item[1]
+    timestamp_list.sort(reverse=True)
+
+    if len(sys.argv) < 2:
+        print('No timestamp or build index specified')
+        return
+    if sys.argv[1] == '-b': 
+        if len(timestamp_list) >= int(sys.argv[2]):
+            timestamp = timestamp_list[int(sys.argv[2])]
+        else:
+            print('No such build index')
+            return
+    else:
+        if sys.argv[1] in timestamp_list:
+            timestamp = sys.argv[1]
+        else:
+            print('No such timestamp')
+            return
+
+    create_chart(list(create_fs_tree('poky/build/tmp/buildstats'+timestamp)), timestamp_list)
 
 
 if __name__ == '__main__':
     main()
-
