db426f404023569915c1c76e061a33f856d5d0f1
feyhoa <eabutylo@stud.eltech.ru>
2024-03-07 16:26:37 +0300

    fix buildroot dockerfile

--- src/buildroot-build/create_image.sh
+++ src/buildroot-build/create_image.sh
@@ -2,20 +2,10 @@
 
 image=$(docker images --filter "reference=buildroot-image*" --format "{{.Repository}}:{{.Tag}}")
 
-
 if [[ -n "$image" ]]
 then
-  echo "Found: $image. Trying to delete."
   docker rmi "$image"
 fi
 
-if [[ -n "$1" ]]
-then
-  version="$1"
-else
-  version="unselected"
-fi
-
-echo "Creating: buildroot-image:$version"
-docker build --build-arg host_uid=$(id -u) --build-arg host_gid=$(id -g) --tag "buildroot-image:$version" .
+docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) --tag "buildroot-image" .
 
--- src/buildroot-build/run_container.sh
+++ src/buildroot-build/run_container.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-
 container=$(docker ps -a -f name=buildroot --format "{{.Names}}")
 
 if [[ -n "$container" ]]
 then 
-  echo "Container already exists."
   docker start -i "$container"
   exit 0
 fi
@@ -15,7 +13,6 @@ image=$(docker images --filter "reference=buildroot-image*" --format "{{.Reposit
 
 if [[ -n "$image" ]]
 then
-  echo "Found: $image."
   docker run -it --name buildroot -v $PWD/assembly:/home/buildroot_user/project/assembly "$image"
 else
   echo "The image wasn't found."
