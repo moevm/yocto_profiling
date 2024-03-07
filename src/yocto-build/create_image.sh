d8574067467e31de27000be8a8503ee4d5390176
feyhoa <eabutylo@stud.eltech.ru>
2024-03-07 14:53:15 +0300

    fix dockerfiles and add docker-compose example

--- src/yocto-build/create_image.sh
+++ src/yocto-build/create_image.sh
@@ -2,20 +2,10 @@
 
 image=$(docker images --filter "reference=yocto-image*" --format "{{.Repository}}:{{.Tag}}")
 
-
 if [[ -n "$image" ]]
 then
-  echo "Found: $image. Trying to delete."
   docker rmi "$image"
 fi
 
-if [[ -n "$1" ]]
-then
-  version="$1"
-else
-  version="unselected"
-fi
-
-echo "Creating: yocto-image:$version"
-docker build --build-arg host_uid=$(id -u) --build-arg host_gid=$(id -g) --tag "yocto-image:$version" .
+docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) --tag "yocto-image" .
 
--- src/yocto-build/run_container.sh
+++ src/yocto-build/run_container.sh
@@ -4,7 +4,6 @@ container=$(docker ps -a -f name=yocto_project --format "{{.Names}}")
 
 if [[ -n "$container" ]]
 then 
-  echo "Container already exists."
   docker start -i "$container"
   exit 0
 fi
@@ -14,10 +13,7 @@ image=$(docker images --filter "reference=yocto-image*" --format "{{.Repository}
 
 if [[ -n "$image" ]]
 then
-  echo "Found: $image."
   docker run -it --name yocto_project -v $PWD/assembly:/home/yocto_user/project/assembly "$image"
 else
   echo "The image wasn't found."
 fi
-
-
