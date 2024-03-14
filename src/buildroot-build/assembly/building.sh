e974b493f1ef16dc366589526a781a6121045800
feyhoa <eabutylo@stud.eltech.ru>
2024-03-14 16:37:14 +0300

    buildroot to compose

--- src/buildroot-build/assembly/building.sh
+++ src/buildroot-build/assembly/building.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
 
-cd $BUILDROOT_INSTALL_PATH/buildroot
+cd $BUILDROOT_INSTALL_PATH/assembly
 
+if [ ! -d "./buildroot" ];
+then
+  git clone https://github.com/buildroot/buildroot.git buildroot
+fi
+
+
+cd $BUILDROOT_INSTALL_PATH/assembly/buildroot
 branch=$(git branch --show-current)
 
 if [ "$branch" != "my-buildroot" ];
@@ -12,7 +19,6 @@ then
 fi
 
 make O=$BUILDROOT_INSTALL_PATH/assembly/build raspberrypi3_defconfig
-
 
 cd $BUILDROOT_INSTALL_PATH/assembly/build
 
