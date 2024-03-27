b11f77cee7311f9984afee10ffeac68af7abc5eb
feyhoa <eabutylo@stud.eltech.ru>
2024-03-27 21:34:15 +0300

    fix instr and del test files

--- src/yocto-build/assembly/test/lsof_i.py
+++
@@ -1,6 +0,0 @@
-import psutil
-
-# Получаем список всех сетевых соединений на системе
-connections = psutil.net_connections(kind='all')
-for conn in connections:
-    print(f"Family: {conn.family}, Local Address: {conn.laddr}, Remote Address: {conn.raddr}, Status: {conn.status}")
