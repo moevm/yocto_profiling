93cfab809bf7fca99e5e572e58dec13aaa47fa35
feyhoa <eabutylo@stud.eltech.ru>
2024-03-27 22:06:58 +0300

    fix script

--- src/yocto-build/run_container.sh
+++ src/yocto-build/run_container.sh
@@ -3,7 +3,7 @@
 STAGE_VAR="stage_run" docker compose up -d
 docker container exec -it yocto_project /bin/bash
 
-docker-compose stop
+docker compose stop
 
 # Shutdown system = `Ctrl + A`, press `X`
 # Alternatively = `Ctrl + A`, press `C`, type `quit`
