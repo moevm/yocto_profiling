a0f585475a1d9e2ffb55756c48da7e19717cb876
Konstantin Evgenievich Kondratenko <90711883+KonstantinKondratenko@users.noreply.github.com>
2024-04-25 22:17:09 +0300

    Update setup_ftp_server.py

--- wiki/yocto_cache/examples/setup_ftp_server.py
+++ wiki/yocto_cache/examples/setup_ftp_server.py
@@ -3,7 +3,7 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
-def setup_ftp():
+def setup_ftp(path):
     authorizer = DummyAuthorizer()
     authorizer.add_anonymous(os.getcwd())
     handler = FTPHandler
@@ -12,7 +12,7 @@ def setup_ftp():
     server.serve_forever()
 
     # обхов дерева текущей директории и выгрузка файлов
-    for root, dirs, files in os.walk(os.getcwd()):
+    for root, dirs, files in os.walk(path):
         for file in files:
             with open(os.path.join(root, file), 'rb') as f:
                 handler.on_file_received(f)
@@ -23,4 +23,7 @@ def setup_ftp():
 
 
 if __name__ == '__main__':
-    setup_ftp()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', type=str, default=os.getcwd(), help='Путь к папке (по умолчанию: текущая рабочая директория)')
+    args = parser.parse_args()
+    setup_ftp(args.path)
