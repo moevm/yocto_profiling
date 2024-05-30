0926424fc42fc054dba9ea1f3aadd78bb571d87f
KonstantinKondratenko <sider123456789101112131415@gmail.com>
2024-05-30 20:32:20 +0300

    fix compose_local_conf

--- src/setup_servers/compose_local_conf.py
+++ src/setup_servers/compose_local_conf.py
@@ -14,28 +14,15 @@ def read_file(name:str):
         return None, None
 
 
-def generate_and_write_strings(file_path:str, base_url:str, start_port:int, start_string:str, target_str:str, num:int):
-    # Открываем файл для записи
-    with open(file_path, 'w') as file:
-        file.write(start_string)
-        file.write('\n')
-        for i in range(start_port, start_port + num):
-            mirror_url = base_url.replace("n", str(i))
-            file.write(f'file://.* {mirror_url}/{target_str}/PATH;downloadfilename=PATH')
-            if i < start_port + num - 1:
-                file.write('\n')
-            else:
-                file.write('"')
-
-def generate_and_strings(out:str, base_url:str, start_port:int, start_string:str, target_str:str, num:int):
+def generate_strings(out:str, base_url:str, start_port:int, start_string:str, target_str:str, num:int):
     out += start_string + '\n'
 
     for i in range(start_port, start_port + num):
         mirror_url = base_url.replace("n", str(i))
-        tmp = f'file://.* {mirror_url}/{target_str}/PATH;downloadfilename=PATH'
+        tmp = f'file://.* {mirror_url}/server_folder_{i}/{target_str}/PATH;downloadfilename=PATH'
         out += tmp
         if i < start_port + num - 1:
-            out += '\n'
+            out += ' \\ \n'
         else:
             out += '"\n\n'
 
@@ -54,11 +41,11 @@ if __name__ == '__main__':
     file_path = "./файлу.txt"
     base_url = "http://10.138.70.218:n"
 
-    start_dl = r'SOURCE_MIRROR_URL ?= "\ '
+    # start_dl = r'SOURCE_MIRROR_URL ?= "\ '
     start_ss = r'SSTATE_MIRRORS ?= "\ '
-
-    a = generate_and_strings(out='', base_url=base_url, start_port=start, start_string=start_dl, target_str='downloads',num=num)
-    a = generate_and_strings(out=a, base_url=base_url, start_port=start, start_string=start_ss, target_str='sstate-cache',num=num)
+    a = ''
+    # a = generate_and_strings(out='', base_url=base_url, start_port=start, start_string=start_dl, target_str='downloads',num=num)
+    a = generate_strings(out=a, base_url=base_url, start_port=start, start_string=start_ss, target_str='sstate-cache',num=num)
 
     a += 'BB_HASHSERVE = "auto" \n'  
     a += 'BB_HASHSERVE_UPSTREAM = "<ip>:<port>" \n'   
