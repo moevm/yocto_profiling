c79888e6b9268083d25644e64b754bc4d32625b8
KonstantinKondratenko <sider123456789101112131415@gmail.com>
2024-05-31 16:39:04 +0300

    add comment in run.sh

--- src/setup_servers/scripts/run.sh
+++ src/setup_servers/scripts/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
+# The argument needs to be passed - the server identification is based on port
 PORT=$1
 
 if [ -z "$PORT" ]; then
     echo "Usage: $0 <port>"
+    echo "The argument needs to be passed - the server identification is based on port"
     exit 1
 fi
 
