fbb7b0d9b5b27cd03c443c4a08dd9412cb05edf2
feyhoa <egorbutylo@mail.ru>
2024-06-13 15:02:04 +0300

    container creating added

--- servers/main.py
+++ servers/main.py
@@ -1,22 +1,27 @@
 import os
 import sys
-from typing import Optional
+from typing import Optional, Union
 
 import docker
 import docker.errors
+from docker.models.containers import Container
 
 
 def connect_to_docker() -> docker.DockerClient:
     try:
         cl = docker.from_env()
-    except docker.errors.DockerException:
-        sys.exit("An error occurred while retrieving docker information, check that docker is installed and running.")
+    except docker.errors.DockerException as e:
+        raise docker.errors.DockerException(
+            f"An error occurred while retrieving docker information, check that docker is installed and running.\n{e}"
+        )
 
     return cl
 
 
 def variables_check(*, start_port: int = 9000, count_of_servers: int = 4) -> tuple[int, int]:
     def checks() -> bool:
+        nonlocal start_port, count_of_servers
+
         if (int != type(start_port)) or (type(count_of_servers) != int):
             return False
         if start_port < 1024:
@@ -27,16 +32,16 @@ def variables_check(*, start_port: int = 9000, count_of_servers: int = 4) -> tup
         return True
 
     if not checks():
-        sys.exit('Variables check for PORTS and SERVER COUNT failed!')
+        raise ValueError('Variables check for PORTS and SERVER COUNT failed!')
     return start_port, count_of_servers
 
 
-def find_image(cl: docker.DockerClient, image: str) -> bool:
+def find_image(cl: docker.DockerClient, image: str) -> None:
     try:
         cl.images.get(image)
-    except docker.errors.ImageNotFound:
-        return False
-    return True
+    except docker.errors.ImageNotFound as e:
+        raise e
+    return
 
 
 def pull_reqs_images(cl: docker.DockerClient, *, images: Optional[list[str]] = None) -> None:
@@ -44,9 +49,7 @@ def pull_reqs_images(cl: docker.DockerClient, *, images: Optional[list[str]] = N
         return
 
     for image in images:
-        if find_image(cl, image):
-            continue
-
+        find_image(cl, image)
         try:
             cl.images.pull(image)
         except docker.errors.APIError as e:
@@ -57,7 +60,7 @@ def pull_reqs_images(cl: docker.DockerClient, *, images: Optional[list[str]] = N
 
 def sstate_dir_check(*, path: str = './build/sstate-cache') -> str:
     if not (os.path.isdir(path) and os.listdir(path)):
-        sys.exit('Directory with sstate-cache was not found or it is empty!')
+        raise OSError('Directory with sstate-cache was not found or it is empty!')
     return path
 
 
@@ -65,7 +68,8 @@ def create_volume_list() -> tuple[list[str], str]:
     global SSTATE_DIR_PATH
 
     def create_volume(cache_dir: str) -> str:
-        return f"{SSTATE_DIR_PATH}/{cache_dir}/:/sstate-cache/{cache_dir}/"
+        nonlocal SSTATE_DIR_PATH
+        return f"{SSTATE_DIR_PATH}/{cache_dir}/:/app/sstate-cache/{cache_dir}/"
 
     volume_list, universal_volume = [], ''
     for elem in os.listdir(SSTATE_DIR_PATH):
@@ -79,23 +83,69 @@ def create_volume_list() -> tuple[list[str], str]:
         volume_list.append(create_volume(elem))
 
     if not (volume_list and universal_volume):
-        sys.exit('Volume list and universal volume was not found or it is empty!')
+        raise Exception('Volume list and universal volume was not found or it is empty!')
 
     return volume_list, universal_volume
 
 
 def create_containers(cl: docker.DockerClient, *,
-                      image: str = 'parted-sstate-cache:latest',
-                      vol: tuple[list[str], str]) -> None:
-    pass
+                      image: str,
+                      vol: tuple[list[str], str]) -> tuple[Container, ...]:
+    global COUNT_OF_SERVERS, START_PORT
+    parted_vol, universal_vol = vol
+
+    find_image(cl, image)
+
+    def create(*, name: str, port: dict[str, int], volume: Union[list[str], str]) -> Container:
+        nonlocal image
+
+        try:
+            container = cl.containers.create(
+                image,
+                name=f"cache-{name}",
+                ports=port,
+                volumes=volume,
+                read_only=True
+            )
+        except docker.errors.APIError as e:
+            raise e
+
+        return container
+
+    def create_port(port: int) -> dict[str, int]:
+        return {f'{port}/tcp': port}
+
+    # create container for universal cache dir
+    containers: list[Container] = [
+        create(name='universal', port=create_port(START_PORT + COUNT_OF_SERVERS - 1), volume=universal_vol)
+    ]
+
+    # TODO сделать деление по (COUNT_OF_SERVERS - 1) серверам
+    # create containers for parted cache
+    for i in range(COUNT_OF_SERVERS - 1):
+        containers.append(create(name=f'part-{i}', port=create_port(START_PORT + i), volume=[]))
+
+    return tuple(containers)
 
 
-def start_containers(cl: docker.DockerClient) -> None:
-    pass
+def start_containers(cl: docker.DockerClient, containers: tuple[Container, ...]) -> None:
+    # containers[i].start()
+    ...
 
 
-def build_base_image(cl: docker.DockerClient) -> None:
-    cl.images.build(path='.', dockerfile='Dockerfile', tag=f'parted-sstate-cache:latest', forcerm=True)
+def kill_containers(cl: docker.DockerClient, containers: tuple[Container, ...]) -> None:
+    ...
+
+
+def build_base_image(cl: docker.DockerClient) -> str:
+    try:
+        image, _ = cl.images.build(path='.', dockerfile='Dockerfile', tag=f'parted-sstate-cache:latest', forcerm=True)
+    except (docker.errors.BuildError, docker.errors.APIError) as e:
+        sys.exit(f'An error occurred while building base image! {e}')
+    except TypeError:
+        sys.exit('Neither path nor fileobj was specified.')
+
+    return image.tags[0]
 
 
 if __name__ == "__main__":
@@ -104,11 +154,7 @@ if __name__ == "__main__":
 
     client = connect_to_docker()
     pull_reqs_images(client, images=['alpine:3.18'])
-    build_base_image(client)
+    image_name = build_base_image(client)
 
     volumes = create_volume_list()
-    create_containers(client, vol=volumes)
-    start_containers(client)
-
-    # get container like:
-    # container = client.containers.get('45e6d2de7c54')
+    create_containers(client, image=image_name, vol=volumes)
