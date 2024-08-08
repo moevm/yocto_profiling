de351594f87b622019bd304e34a872154046770a
feyhoa <eabutylo@stud.eltech.ru>
2024-08-08 18:41:33 +0300

    update patching script, add poky to python script

--- src/patching.sh
+++ src/patching.sh
@@ -4,26 +4,8 @@ SOURCE_DIR=$(dirname $0)
 POKY_DIR=$SOURCE_DIR/poky
 PATCHES_PATH=$SOURCE_DIR/yocto_patches
 
-# PARSE $PATCHES_PATH/patches.cfg
-# GET LIST OF AVAILABLE PATCHES FROM patches.cfg
-# config type: json
-#
-# {
-# 	"patch_name": {
-# 		"path": "path_to_dir",
-# 		"file": "file_to_patch"
-# 	},
-# 	...
-# }
-
-
 # INPUT
 # GET PATCHES LIST FROM USER
 
-# VALIDATE
-# CHECK IF EXISTS AND AVAILABLE
-
-# BUILD
-# MOVE PATCH TO HIS PATH LIKE .../poky/bitbake/lib/bb/
-# APPLY PATCH: patch -p1 file < file.patch
+# python3 $PATHCHES_PATH/main.py --poky-path $POKY_DIR --dir-path $PATCHES_PATH --patch ... --patch ...
 
--- src/yocto_patches/main.py
+++ src/yocto_patches/main.py
@@ -5,28 +5,34 @@ import json as _json
 from pathlib import Path as _Path
 
 
-if patches_json_path.exists():
-	try:
-		_json.loads()
-
-
-def _validate_path(dir_path: _Optional[str], patches_filename: str) -> tuple[_Path, _Path]:
-	if path is None:
-		raise ValueError("Error: The path was not received. Use --path to pass this value.")
+def _validate_path(poky_path: _Optional[str], dir_path: _Optional[str], patches_filename: str) -> tuple[_Path, _Path]:
+	if poky_path is None:
+		raise ValueError("Error: The poky path was not received. Use --poky-path to pass this value.")
+	if dir_path is None:
+		raise ValueError("Error: The path was not received. Use --dir-path to pass this value.")
 	if not ".json" in patches_filename:
 		raise ValueError("Error: Json file was expected.")
 	
+	poky_dir = _Path(poky_path)
 	patches_dir = _Path(dir_path)
 	patches_file = patches_dir / patches_filename
+	
+	if not poky_dir.exists() and poky_dir.isdir():
+		raise ValueError(f"Error: This directory does not exist, or the path does not point to a directory: {poky_dir}")
 	if not patches_dir.exists() and patches_dir.isdir():
 		raise ValueError(f"Error: This directory does not exist, or the path does not point to a directory: {dir_path}")
 	if not patches_file.exists():
 		raise ValueError(f"Error: This file does not exist: {str(patches_file)})
-	return patches_dir, patches_file
+	return poky_dir, patches_dir, patches_file
 
 
-def patching(dir_path: _Optional[str], patches_filename: str, patches: _Optional[list[str]]) -> _Optional[list[tuple[str, str, str]]]:
-	patches_dir, patches_file = _validate_path(dir_path, patches_filename)
+def patching(args) -> _Optional[list[tuple[str, str, str]]]:
+	poky_path = args.poky_path
+	dir_path = args.dir_path
+	patches_filename = args.patches_filename
+	patches = args.patches
+		
+	poky_dir, patches_dir, patches_file = _validate_path(poky_path, dir_path, patches_filename)
 	
 	if not patches:
 		return
@@ -47,14 +53,19 @@ def patching(dir_path: _Optional[str], patches_filename: str, patches: _Optional
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
+	parser.add_argument('--poky-path', dest="poky_path", type=str, default=None)
 	parser.add_argument('--dir-path', dest="dir_path", type=str, default=None)
 	parser.add_argument('--patches-filename', dest="patches_filename", type=str, default="patches.json")
 	parser.add_argument('--patch', dest="patches", action='append', type=str, default=None)
 
 	args = parser.parse_args()
-	patches = patching(dir_path=args.dir_path, patches_filename=args.patches_filename, patches=args.patches)
+	patches = patching(args)
 	
 	if patches is None:
 		print("Nothing to patch!")
 		sys.exit(0)
+		
+	# TODO MOVE PATCH TO HIS PATH LIKE .../poky/bitbake/lib/bb/
+	# TODO APPLY PATCH: patch -p1 file < file.patch
+
 	
