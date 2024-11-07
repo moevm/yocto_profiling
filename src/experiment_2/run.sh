d3cb2ea7a7c5d8bbd4d94f886bd371fc3d88e9be
KonstantinKondratenko <sider123456789101112131415@gmail.com>
2024-11-07 17:37:38 +0300

    full exp

--- src/experiment_2/run.sh
+++ src/experiment_2/run.sh
@@ -53,62 +53,36 @@ do
     runtime=$((end-start))
     echo -e "[CACHE SERVERS $i]" >> ../../times/times
     echo -e "|$runtime|" >> ../../times/times
-    # cd .. && rm -rf build
+    cd .. && rm -rf build
+
+done
+
+echo "reset params!"
+
+git restore meta/classes-global/sstate.bbclass
+cd ..
+cp -f async_filter_with_time.patch ./poky/
+cd poky 
+git apply async_filter_with_time.patch
 
 
 
+for (( i=2; i<$max_servers; i+=$step ))
+do
+    source oe-init-build-env
+    # copy config
+    cp -f ../../configs/${i}/local.conf ./conf
 
+    start=`date +%s`
+    bitbake core-image-minimal >> ../../times/Patch_$i
+    # bitbake core-image-minimal
 
+    end=`date +%s`
+    runtime=$((end-start))
+    echo -e "[CACHE SERVERS $i]" >> ../../times/times
+    echo -e "|$runtime|" >> ../../times/times
+    cd .. && rm -rf build
 
-	# # подъём серверов
- 	# ssh $hash_usr@$hash_ip "cd $hash_desktop_path/test/hash_server_setuper && ./start_hash.sh $hash_port"  2> /dev/null
-	# echo -e "\n\nHash server started at $hash_ip:$hash_port"
-
-	
-
-
-	# # ssh $cache_usr@$cache_ip "cd $CACHE_SERVER_WORKDIR && ./experiment/cache_containers.sh start $cache_start_port $i" > /dev/null
-	# # делаем распределение кэша только на 2 сервера
-	# if [ $i -eq 2 ]; then
-	# 	# scp -rf ../../../src/ $cache_usr@$cache_ip:$CACHE_SERVER_WORKDIR
-	# 	# scp -r ../../../src/ user@10.138.70.7:/home/user/Desktop/test/
-	# 	echo "AAAAAAAAAAAAAAAAAAAAAAAA"
-	# 	ssh $cache_usr@$cache_ip "cd $CACHE_SERVER_WORKDIR && ./experiment/cache_containers.sh start $cache_start_port $i" > /dev/null
-	# 	echo "BBBBBBBBBBBBBBBBBBBBBBBB"
-
-	# fi
-	# cd $SCRIPT_DIR/auto_conf && python3 set_num_ports.py --cache_num_port $i
-	# echo -e "Building Yocto on host with $i servers: START.\n"
-
-    # cp -f $BASE_DIR/build/conf/experiment.conf $SCRIPT_DIR/auto_conf/conf/local.conf
-    # cd $SCRIPT_DIR/auto_conf && python3 auto_compose_local_conf.py
-	# echo -e "[CACHE SERVERS $i]" >> $EXPERIMENT_DIR/"times"
-	# for j in 1 2
-	# do
-	# 	cp -f $SCRIPT_DIR/auto_conf/conf/local.conf $BASE_DIR/build/conf/local.conf
-	# 	filename="test_${i}_${j}"
-	# 	start=`date +%s`
-	# 	cd $SRC_DIR && ./entrypoint.sh build_yocto_image --no-layers > $EXPERIMENT_DIR/"$filename"
-	# 	end=`date +%s`
-
-	# 	runtime=$((end-start))
-	# 	echo -e "REPEAT $j TIME: $runtime" >> $EXPERIMENT_DIR/"times"
-	# 	cat $EXPERIMENT_DIR/"$filename" | grep "Parsing recipes: 100% || Time:" >> $EXPERIMENT_DIR/"times"
-	# 	cat $EXPERIMENT_DIR/"$filename" | grep "Checking sstate mirror object availability: 100% || Time:" >> $EXPERIMENT_DIR/"times"
-
-	# 	echo -e "Remove build folder\n"
-	# 	cd $SRC_DIR/yocto-build/assembly && rm -rf ./build
-
-	# 	sleep 15
-	# done
-	# echo -e "" >> $EXPERIMENT_DIR/"times"
-	# echo -e "Building Yocto on host: DONE.\n"
-	# ssh $hash_usr@$hash_ip "cd $hash_desktop_path/test/hash_server_setuper && ./manipulate_hash.sh stop" 2> /dev/null
-	# ssh $hash_usr@$hash_ip "cd $hash_desktop_path/test/hash_server_setuper && ./manipulate_hash.sh rm" 2> /dev/null
-	# # не убиваем контейнер
-	# # ssh $cache_usr@$cache_ip "cd $CACHE_SERVER_WORKDIR && ./experiment/cache_containers.sh kill" 2> /dev/null
-	
-	# sleep 25
 done
 
 
